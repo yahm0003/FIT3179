2d011cfdf6e545ee78aa3575ce99ce69345cb06d
yahm0003 <110844494+yahm0003@users.noreply.github.com>
2024-10-15 22:53:37 +1100

    viz 2 upload
    all files

---
+++ vega_lite_vis.js
@@ -0,0 +1,15 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#map", vg_1, { "actions": false }).then(function (result) {
+}).catch(console.error);
+
+var vg_2 = "area.vg.json";
+vegaEmbed("#line", vg_2, { "actions": false }).then(function (result) {
+}).catch(console.error);
+
+var vg_3 = "bar.vg.json";
+vegaEmbed("#bar", vg_3, { "actions": false }).then(function (result) {
+}).catch(console.error);
+
+var vg_4 = "donut.vg.json";
+vegaEmbed("#donut", vg_4, { "actions": false }).then(function (result) {
+}).catch(console.error);
